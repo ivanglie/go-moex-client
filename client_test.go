5826f42b222dbda3fe83f5f6c4b1b15d1ae9325a
ivanglie <ivanglie@me.com>
2022-08-19 12:25:02 +0300

    Create test.yml
    Fix tests, update workflow
    
    Add some tests
    
    Add workflow

--- client_test.go
+++ client_test.go
@@ -1,12 +1,34 @@
 package moex
 
 import (
+	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestClient(t *testing.T) {
+	if statusCode, err := func() (statusCode int, err error) {
+		req, err := http.NewRequest("GET", baseURL+"/index.xml", nil)
+		if err != nil {
+			return
+		}
+
+		client := http.Client{Timeout: 3 * time.Second}
+		resp, err := client.Do(req)
+		if err != nil {
+			return
+		}
+		defer resp.Body.Close()
+		statusCode = resp.StatusCode
+
+		return
+	}(); err != nil || statusCode != http.StatusOK {
+		t.Log(statusCode, err)
+		t.SkipNow()
+	}
+
 	client := NewClient()
 	rate, err := client.GetRate(USDRUB)
 	assert.Nil(t, err)
