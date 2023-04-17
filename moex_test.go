509e0319cbfe2ed72e238489b08a4b652395b8cf
ivanglie <ivanglie22@gmail.com>
2023-04-17 17:32:49 +0300

    Add test

--- moex_test.go
+++ moex_test.go
@@ -54,7 +54,6 @@ func Test_getRate(t *testing.T) {
 	}
 
 	r, err = getRate("C1", fetchFunc)
-	t.Log(r, err)
 	assert.Error(t, err)
 	assert.Equal(t, float64(0), r)
 
@@ -93,4 +92,24 @@ func Test_getRate(t *testing.T) {
 	r, err = getRate("C1", fetchFunc)
 	assert.Error(t, err)
 	assert.Equal(t, float64(0), r)
+}
+
+func TestCurrency_String(t *testing.T) {
+	s := Currency{
+		values: []currency{
+			{
+				Charsetinfo: struct {
+					Name string "json:\"name\""
+				}{
+					Name: "UTF-8",
+				},
+			},
+		},
+	}
+
+	assert.NotNil(t, s.String())
+
+	// Empty values
+	s.values = []currency{}
+	assert.Empty(t, s.String())
 }
